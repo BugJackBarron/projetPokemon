be86b6d3cc4b77c80cdb6b414fef179da1c65840
Fabien Vergniaud <47561386+BugJackBarron@users.noreply.github.com>
2021-12-10 12:01:56 +0100

    MAJ

--- csvToSqlite.py
+++ csvToSqlite.py
@@ -10,6 +10,7 @@ with open('pokemon.csv', newline='', encoding='utf8') as csvfile:
     CREATE TABLE IF NOT EXISTS Pokemon (
     id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,
     name TEXT,
+    imageName TEXT,
     type1 TEXT,
     type2 TEXT,
     total INTEGER,
@@ -23,13 +24,24 @@ with open('pokemon.csv', newline='', encoding='utf8') as csvfile:
     legendary BOOLEAN)
     """)
     for line in list(reader)[1:] :
-        print(line)
+        if " "in line[1] :
+            splited = line[1].split(" ")
+            print(splited)
+            for t in splited[1: ] :
+                
+                if len(t)>1 and t in splited[0] :
+                    newl = splited[0].split(t)[-1]
+                    name = newl+" "+" ".join(splited[1:])
+                    imgname = "-".join(name.split(" "))
+        else :
+                name= line[1]
+                imgname= line[1]
         req = f"""
-    INSERT INTO Pokemon (name, type1, type2, total, hp,
+    INSERT INTO Pokemon (name, imagename, type1, type2, total, hp,
     attack, defense,sp_attack, sp_defense, speed, generation, legendary
-    ) VALUES (?,?,?,?,?,?,?,?,?,?,?,?);
+    ) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?);
         """
-        curseur.execute(req,[line[1].lower(), *line[2:]] )
+        curseur.execute(req,[name.lower(),imgname.lower(), *line[2:]] )
 con.commit()
 con.close()
         
